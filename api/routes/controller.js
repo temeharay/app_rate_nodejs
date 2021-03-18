21486b6869e1e4942a57b0aa0706c32f14b909c6
temeharay <adellasmelash22@gmail.com>
2021-03-18 11:14:18 -0500

    api hit counter added

--- api/routes/controller.js
+++ api/routes/controller.js
@@ -2,19 +2,24 @@ const express = require('express');
 const app = require('../../app');
 const router = express.Router();
 const envmw = process.env.env_name;
-const appname = process.env.appname;
+const appname = process.env.app_name;
+var counter = 1;
 
 router.get('/appname', (req, res, next) => {
-    console.log('app name: '+appname);
+    console.log('app name: ' + appname);
     res.status(200).json({
-        message: 'app name is rate node js'
+        message: 'app name is ' + appname,
+        counts: '' + counter
     });
+    counter++;
 });
 router.get('/magicword', (req, res, next) => {
-    console.log('magic word: '+envmw);
+    console.log('magic word: ' + envmw);
     res.status(200).json({
-        message: envmw+' - magic word is aprika daprika booooo!'
+        message: envmw + ' - magic word is aprika daprika booooo!',
+        counts: '' + counter
     });
+    counter++;
 });
 
 module.exports = router;
